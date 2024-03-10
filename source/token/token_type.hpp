1bcfe1ee707b2aed9b14a7a7d13b9fa8a5000146
wandvvs <wandvvs@github.com>
2024-03-10 23:37:43 +0300

    plus, minus, mul and div tokens

--- source/token/token_type.hpp
+++ source/token/token_type.hpp
@@ -15,6 +15,14 @@ enum class TokenType
     LET,
     ASSIGN,
     IDENTIFIER,
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    FUNC,
+    RETURN_TYPE,
+    LEFT_BRACE,
+    RIGHT_BRACE
 };
 
 inline std::string token_type_to_string(TokenType m_type)
@@ -25,6 +33,22 @@ inline std::string token_type_to_string(TokenType m_type)
             return "EXIT";
         case TokenType::INT_LITERAL:
             return "INT_LITERAL";
+        case TokenType::FUNC:
+            return "FUNC";
+        case TokenType::PLUS:
+            return "PLUS";
+        case TokenType::MINUS:
+            return "MINUS";
+        case TokenType::DIV:
+            return "DIV";
+        case TokenType::MUL:
+            return "MUL";
+        case TokenType::RETURN_TYPE:
+            return "RETURN_TYPE";
+        case TokenType::LEFT_BRACE:
+            return "LEFT_BRACE";
+        case TokenType::RIGHT_BRACE:
+            return "RIGHT_BRACE";
         case TokenType::LPAREN:
             return "LPAREN";
         case TokenType::RPAREN:
