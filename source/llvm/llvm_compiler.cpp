485b55dc46fedf169e9b29c51a770d4f032ded23
wandvvs <wandvvs@github.com>
2024-03-11 19:02:42 +0300

    methods: process_extern_std, process_writeln. some refactor

--- source/llvm/llvm_compiler.cpp
+++ source/llvm/llvm_compiler.cpp
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <cstdlib>
 #include <llvm-16/llvm/IR/BasicBlock.h>
+#include <llvm-16/llvm/IR/Constant.h>
 #include <llvm-16/llvm/IR/DerivedTypes.h>
 #include <llvm-16/llvm/IR/Function.h>
 #include <llvm-16/llvm/IR/Value.h>
@@ -12,16 +13,28 @@ LLVMCompiler::LLVMCompiler(const std::string& module_name, TokenBuffer tokens_bu
     {
     }
 
-TokenType LLVMCompiler::current_type() const { return m_tokens_buffer.m_current.value().get_type(); }
-std::string LLVMCompiler::current_value() const { return m_tokens_buffer.m_current.value().get_value().value(); }
+void LLVMCompiler::process_extern_std()
+{
+    m_tokens_buffer.move_next();
+    check_token_type(TokenType::SEMICOLON, "Expected ';' after extern");
+
+    m_printf_type = llvm::FunctionType::get(llvm::Type::getInt32Ty(m_context), {llvm::Type::getInt8PtrTy(m_context)}, true);
+    m_printf_func = llvm::Function::Create(m_printf_type, llvm::Function::ExternalLinkage, "printf", m_module);   
+
+    std_externed = true;
+    m_tokens_buffer.move_next();
+}
+
+TokenType LLVMCompiler::current_type() const { return m_tokens_buffer.m_current.get_type(); }
+std::string LLVMCompiler::current_value() const { return m_tokens_buffer.m_current.get_value(); }
 
 void LLVMCompiler::check_token_type(TokenType expected_type, const std::string& error_message) const
 {
-    if(!m_tokens_buffer.m_current.has_value() || current_type() != expected_type)
+    if(current_type() != expected_type)
     {
         std::cerr << "Syntax error. " << error_message << std::endl;
 
-        throw std::runtime_error("Syntax error");
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -30,28 +43,14 @@ llvm::Value* LLVMCompiler::get_variable(const std::string& name) const
     if (m_variables.count(name) == 0)
     {
         std::cerr << "Error: Variable '" << name << "' not defined." << std::endl;
-        throw std::runtime_error("Variable not defined.");
+        exit(EXIT_FAILURE);
     }
 
     return m_variables.at(name);
 }
 
-void LLVMCompiler::process_assign() {
-    if (m_variables.find(current_value()) == m_variables.end()) 
-    {
-        std::cerr << "Syntax error. Undefined identifier" << std::endl;
-        throw std::runtime_error("Undefined identifier.");
-    }
-
-    std::string left_identifier_name = current_value();
-
-    m_tokens_buffer.move_next();
-    check_token_type(TokenType::ASSIGN, "Expected '=' after identifier");
-
-    m_tokens_buffer.move_next();
-
-    llvm::Value* right_value = process_expr();
-
+llvm::Value* LLVMCompiler::process_arithmetic(llvm::Value* left_value)
+{
     while (current_type() == TokenType::PLUS || current_type() == TokenType::MINUS ||
            current_type() == TokenType::MUL || current_type() == TokenType::DIV) 
     {
@@ -61,29 +60,99 @@ void LLVMCompiler::process_assign() {
         switch (operation) 
         {
             case TokenType::PLUS:
-                right_value = m_builder.CreateAdd(right_value, temp, "add");
+                left_value = m_builder.CreateAdd(left_value, temp, "add");
                 break;
             case TokenType::MINUS:
-                right_value = m_builder.CreateSub(right_value, temp, "sub");
+                left_value = m_builder.CreateSub(left_value, temp, "sub");
                 break;
             case TokenType::MUL:
-                right_value = m_builder.CreateMul(right_value, temp, "mul");
+                left_value = m_builder.CreateMul(left_value, temp, "mul");
                 break;
             case TokenType::DIV:
-                right_value = m_builder.CreateSDiv(right_value, temp, "div");
+                left_value = m_builder.CreateSDiv(left_value, temp, "div");
                 break;
             default:
                 std::cerr << "Unexpected operator." << std::endl;
                 throw std::runtime_error("Unexpected operator.");
         }
     }
-
-    set_variable(left_identifier_name, right_value);
-    check_token_type(TokenType::SEMICOLON, "Expected for ';'");
-    m_tokens_buffer.move_next();
+    return left_value;
 }
 
+void LLVMCompiler::process_assign() {
+    if(m_constants.find(current_value()) != m_constants.end())
+    {
+        std::cerr << "Syntax error. Variable '" << current_value() << "' is constant and cannot be reassigned" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if (m_variables.find(current_value()) == m_variables.end() && m_string_variables.find(current_value()) == m_string_variables.end()) 
+    {
+        std::cerr << "Syntax error. Undefined identifier" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
+    std::string left_identifier_name = current_value();
+
+    m_tokens_buffer.move_next();
+    check_token_type(TokenType::ASSIGN, "Expected '=' after identifier");
+    m_tokens_buffer.move_next();
+
+    if(current_type() == TokenType::QOUTE)
+    {
+        m_tokens_buffer.move_next();
+        std::string right_string_value = current_value();
+
+        if(m_variables.find(left_identifier_name) != m_variables.end())
+        {
+            m_variables.erase(left_identifier_name);
+
+            std::string string_literal = current_value();
+            llvm::Value* str_ptr = m_builder.CreateGlobalStringPtr(string_literal);
+            llvm::Value* value = str_ptr;
+            m_tokens_buffer.move_next();
+            check_token_type(TokenType::QOUTE, "Expected '\"' after string literal");
+
+            m_string_variables[left_identifier_name] = value;
+            m_tokens_buffer.move_next();
+
+        }
+        else
+        {
+            
+            llvm::Value* str_ptr = m_string_variables[left_identifier_name];
+
+            llvm::Constant* new_str_ptr = m_builder.CreateGlobalStringPtr(right_string_value);
+
+            str_ptr->replaceAllUsesWith(new_str_ptr);
+
+            m_string_variables[left_identifier_name] = new_str_ptr;
+            m_tokens_buffer.move_next();
+                    m_tokens_buffer.move_next();
+        }
+    }
+    else if(current_type() == TokenType::IDENTIFIER && m_string_variables.find(left_identifier_name) != m_string_variables.end())
+    {
+        llvm::Value* str_ptr = m_string_variables[current_value()];
+        llvm::Value* new_str_ptr = m_string_variables[left_identifier_name];
+
+        str_ptr->replaceAllUsesWith(new_str_ptr);
+        m_string_variables[current_value()] = new_str_ptr;
+
+        m_tokens_buffer.move_next();
+    }
+    else
+    {
+    llvm::Value* right_value = process_expr();
+
+    right_value = process_arithmetic(right_value);
+
+    set_variable(left_identifier_name, right_value);
+ 
+    }
+
+   check_token_type(TokenType::SEMICOLON, "Expected for ';'");
+    m_tokens_buffer.move_next();
+}
 
 void LLVMCompiler::set_variable(const std::string& name, llvm::Value* value) { m_variables[name] = value; }
 
@@ -93,33 +162,15 @@ llvm::Value* LLVMCompiler::process_exit()
     check_token_type(TokenType::LPAREN, "Expected for '(");
     m_tokens_buffer.move_next();
 
+    if(current_type() == TokenType::IDENTIFIER && m_string_variables.find(current_value()) != m_string_variables.end())
+    {
+        std::cerr << "Syntax error. Exit argument can`t be string." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
     llvm::Value* return_value = process_expr();
 
-    while (current_type() == TokenType::PLUS || current_type() == TokenType::MINUS ||
-           current_type() == TokenType::MUL || current_type() == TokenType::DIV) 
-    {
-        TokenType operation = current_type();
-        m_tokens_buffer.move_next(); 
-        llvm::Value* temp = process_expr(); 
-        switch (operation) 
-        {
-            case TokenType::PLUS:
-                return_value = m_builder.CreateAdd(return_value, temp, "add");
-                break;
-            case TokenType::MINUS:
-                return_value = m_builder.CreateSub(return_value, temp, "sub");
-                break;
-            case TokenType::MUL:
-                return_value = m_builder.CreateMul(return_value, temp, "mul");
-                break;
-            case TokenType::DIV:
-                return_value = m_builder.CreateSDiv(return_value, temp, "div");
-                break;
-            default:
-                std::cerr << "Unexpected operator." << std::endl;
-                throw std::runtime_error("Unexpected operator.");
-        }
-    }
+    return_value = process_arithmetic(return_value);
 
     check_token_type(TokenType::RPAREN, "Expected ')'");
 
@@ -211,44 +262,170 @@ llvm::Value* LLVMCompiler::process_factor()
     return value;
 }
 
-void LLVMCompiler::process_let() {
+void LLVMCompiler::process_writeln()
+{
     m_tokens_buffer.move_next();
-    check_token_type(TokenType::IDENTIFIER, "Expected identifier after 'let'");
+    check_token_type(TokenType::LPAREN, "Expected '(' after 'writeln'.");
+    m_tokens_buffer.move_next();
+
+    if(current_type() == TokenType::QOUTE)
+    {
+        m_tokens_buffer.move_next();
+
+        check_token_type(TokenType::STRING_LITERAL, "Expected string literal after \"");
+        std::string string_literal = m_tokens_buffer.m_current.get_value();
+
+        m_tokens_buffer.move_next();
+
+        check_token_type(TokenType::QOUTE, "Expected '\"' after string literal");
+        m_tokens_buffer.move_next();
+        check_token_type(TokenType::RPAREN, "Expected ')' after '\"'");
+
+        llvm::Constant* format_str = m_builder.CreateGlobalStringPtr("%s\n");
+
+        m_builder.CreateCall(m_printf_func, {format_str, m_builder.CreateGlobalStringPtr(string_literal)});
+    }
+    else if(current_type() == TokenType::INT_LITERAL)
+    {
+        llvm::Value* value = process_expr();
+
+        value = process_arithmetic(value);
+
+
+        llvm::Value* format_str = m_builder.CreateGlobalStringPtr("%d\n");
+
+        m_builder.CreateCall(m_printf_func, {format_str, value});
+
+    }
+    else if(current_type() == TokenType::IDENTIFIER)
+    {
+        if (m_variables.find(current_value()) != m_variables.end()) 
+        {
+            std::string variable_name = current_value();
+            llvm::Value* value = get_variable(variable_name);
+
+            llvm::Value* format_str = m_builder.CreateGlobalStringPtr("%d\n");
+            m_builder.CreateCall(m_printf_func, {format_str, value});
+        }
+        else if (m_string_variables.find(current_value()) != m_string_variables.end()) 
+        {
+            std::string variable_name = current_value();
+            llvm::Value* value = m_string_variables[variable_name];
+
+            llvm::Value* format_str = m_builder.CreateGlobalStringPtr("%s\n");
+            m_builder.CreateCall(m_printf_func, {format_str, value});
+        }
+        else
+        {
+            std::cerr << "Syntax error. Undefined identifier" << std::endl;
+            exit(EXIT_FAILURE);
+        } 
+    }
+    m_tokens_buffer.move_next();
+    if(current_value() == ")")
+    {
+        m_tokens_buffer.move_next();
+    }
+    check_token_type(TokenType::SEMICOLON, "Expected for ';'");
+    m_tokens_buffer.move_next();
+}
+
+
+void LLVMCompiler::process_mut() {
+    m_tokens_buffer.move_next();
+    check_token_type(TokenType::IDENTIFIER, "Expected identifier after 'mut'");
     std::string variable_name = current_value();
+
+    if (m_constants.find(variable_name) != m_constants.end()) {
+        std::cerr << "Error: Variable '" << variable_name << "' is constant and cannot be reassigned." << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
     m_tokens_buffer.move_next();
     check_token_type(TokenType::ASSIGN, "Expected '=' after identifier");
     m_tokens_buffer.move_next();
 
-    llvm::Value* value = process_expr(); 
+    llvm::Value* value;
+    std::string right_variable_name = current_value();
 
-    while (current_type() == TokenType::PLUS || current_type() == TokenType::MINUS ||
-           current_type() == TokenType::MUL || current_type() == TokenType::DIV) 
+    if(current_type() == TokenType::QOUTE)
     {
-        TokenType operation = current_type();
-        m_tokens_buffer.move_next(); 
-        llvm::Value* temp = process_expr(); 
-        switch (operation) 
-        {
-            case TokenType::PLUS:
-                value = m_builder.CreateAdd(value, temp, "add");
-                break;
-            case TokenType::MINUS:
-                value = m_builder.CreateSub(value, temp, "sub");
-                break;
-            case TokenType::MUL:
-                value = m_builder.CreateMul(value, temp, "mul");
-                break;
-            case TokenType::DIV:
-                value = m_builder.CreateSDiv(value, temp, "div");
-                break;
-            default:
-                std::cerr << "Unexpected operator." << std::endl;
-                throw std::runtime_error("Unexpected operator.");
-        }
+        m_tokens_buffer.move_next();
+        check_token_type(TokenType::STRING_LITERAL, "Expected string literal after '\"'");
+
+        std::string string_literal = current_value();
+        llvm::Value* str_ptr = m_builder.CreateGlobalStringPtr(string_literal);
+        value = str_ptr;
+        m_tokens_buffer.move_next();
+        check_token_type(TokenType::QOUTE, "Expected '\"' after string literal");
+
+        m_string_variables[variable_name] = value;
+        m_tokens_buffer.move_next();
+    }
+    else if (current_type() == TokenType::IDENTIFIER && m_string_variables.find(right_variable_name) != m_string_variables.end())
+    {
+        llvm::Value* str_ptr_a = m_string_variables[current_value()];
+        m_string_variables[variable_name] = str_ptr_a;
+        m_tokens_buffer.move_next();
+    }
+    else
+    {
+        value = process_expr();
+        value = process_arithmetic(value);
+
+        set_variable(variable_name, value);
+    }
+    check_token_type(TokenType::SEMICOLON, "Expected for ';'");
+    m_tokens_buffer.move_next();
+}
+
+void LLVMCompiler::process_const()
+{
+    m_tokens_buffer.move_next();
+    check_token_type(TokenType::IDENTIFIER, "Expected identifier after 'const'");
+    std::string variable_name = current_value();
+
+    if(m_variables.find(variable_name) != m_variables.end())
+    {
+        std::cerr << "Syntax error. Redifinition of '" << variable_name << "'" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    set_variable(variable_name, value);
+
+    m_tokens_buffer.move_next();
+    check_token_type(TokenType::ASSIGN, "Expected '=' after identifier");
+    m_tokens_buffer.move_next();
+
+    llvm::Value* value = nullptr;
+    std::string right_variable_name = current_value();
+
+    if(current_type() == TokenType::QOUTE)
+    {
+        m_tokens_buffer.move_next();
+        check_token_type(TokenType::STRING_LITERAL, "Expected string literal after '\"'");
+
+        std::string string_literal = current_value();
+        llvm::Value* str_ptr = m_builder.CreateGlobalStringPtr(string_literal);
+        value = str_ptr;
+        m_tokens_buffer.move_next();
+        check_token_type(TokenType::QOUTE, "Expected '\"' after string literal");
+
+        m_string_variables[variable_name] = value;
+        m_tokens_buffer.move_next();
+    }
+    else if (current_type() == TokenType::IDENTIFIER && m_string_variables.find(right_variable_name) != m_string_variables.end())
+    {
+        llvm::Value* str_ptr_a = m_string_variables[current_value()];
+        m_string_variables[variable_name] = str_ptr_a;
+        m_tokens_buffer.move_next();
+    }
+    else
+    {
+        value = process_expr();
+        set_variable(variable_name, value);
+    }
+
+    m_constants.insert(variable_name);
     check_token_type(TokenType::SEMICOLON, "Expected for ';'");
     m_tokens_buffer.move_next();
 }
@@ -269,17 +446,38 @@ void LLVMCompiler::generate_main_function()
             llvm::Value* return_value = process_exit();
             m_builder.CreateRet(return_value);
         }
-        if(current_type() == TokenType::LET)
+        if(current_type() == TokenType::EXTERN_STD)
         {
-            process_let();
+            process_extern_std();
         }
-
+        if(current_type() == TokenType::WRITELN)
+        {
+            if(std_externed)
+            {
+                process_writeln();
+            }
+            else
+            {
+                std::cerr << "Syntax error. No finded extern 'std'" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        if(current_type() == TokenType::MUT)
+        {
+            process_mut();
+        }
+        if(current_type() == TokenType::CONST)
+        {
+            process_const();
+        }
         if(current_type() == TokenType::IDENTIFIER)
         {
             process_assign();
         }
         parse_pos++;
     }
+
+    m_builder.CreateRet(llvm::ConstantInt::get(m_builder.getInt64Ty(), 0));
 }
 
 std::string LLVMCompiler::get_llvm_ir_as_string() const
