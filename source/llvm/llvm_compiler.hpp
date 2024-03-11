639f31fbd308b687fd7f1212a0cf61205fcb675e
wandvvs <wandvvs@github.com>
2024-03-11 19:02:11 +0300

    new maps for strings and consts

--- source/llvm/llvm_compiler.hpp
+++ source/llvm/llvm_compiler.hpp
@@ -13,6 +13,7 @@
 #include <unordered_map>
 #include <cstddef>
 #include <stdexcept>
+#include <unordered_set>
 
 class LLVMCompiler
 {
@@ -21,11 +22,16 @@ private:
     llvm::Module m_module;
     llvm::IRBuilder<> m_builder;
     TokenBuffer m_tokens_buffer;
-    std::unordered_map<std::string, llvm::Value*> m_variables;
     size_t parse_pos = 0;
 
     TokenType current_type() const;
     std::string current_value() const;
+
+    std::unordered_map<std::string, llvm::Value*> m_variables;
+    std::unordered_map<std::string, llvm::Value*> m_string_variables;
+    std::unordered_set<std::string> m_constants;
+
+    bool std_externed = false;
 
 public:
     LLVMCompiler(const std::string& module_name, TokenBuffer tokens_buffer);
@@ -34,12 +40,20 @@ public:
     void generate_main_function();
 
     llvm::Value* process_exit();
+    void process_writeln();
+
+    void process_extern_std();
 
     llvm::Value* process_expr();
     llvm::Value* process_term();
     llvm::Value* process_factor();
+    llvm::Value* process_arithmetic(llvm::Value* left_value);
 
-    void process_let();
+    llvm::Function* m_printf_func;
+    llvm::FunctionType* m_printf_type;
+
+    void process_mut();
+    void process_const();
     void process_assign(); 
 
     llvm::Value* get_variable(const std::string& name) const;
